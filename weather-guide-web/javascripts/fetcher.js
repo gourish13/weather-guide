9c55443b3a479903b6bd4e015a543ee34fafd2af
Gourish Sadhu <sadhu5gourish@outlook.com>
2020-10-23 21:22:32 +0530

    working api test ... OK

--- weather-guide-web/javascripts/fetcher.js
+++ weather-guide-web/javascripts/fetcher.js
@@ -2,7 +2,6 @@ const apiKey = "656fc6ced8b49f7b5d51f8e68d78e400"
 var weatherData = {}
 
 function weatherFetch(cityName) {
-        // fetch(`api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
         fetch(`https:\/\/cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
                 .then(function(res) {
                         return res.json();
@@ -14,12 +13,9 @@ function weatherFetch(cityName) {
                         console.log("Error in fetching weather data.")
                         console.log(err);
                 })
-        console.log(weatherData)
-        // return weatherData
 }
 
 function storeData(data) {
-        console.log(data)
         weatherData = data
         console.log(weatherData)
 } 
