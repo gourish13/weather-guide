f6dc35b1a4f0b802b37992577c81e48ba4566ae2
Gourish Sadhu <sadhu5gourish@outlook.com>
2020-10-25 20:04:57 +0530

    working on card template

---
+++ weather-guide-web/javascripts/weathercard.js
@@ -0,0 +1,20 @@
+// Weather Card Template Generator
+
+function setCard() {
+        let city = 'Asansol'
+        let country = 'IN'
+        let weatherCond = 'Clear'
+        let icon = '01n'
+        let temp = 24
+        let unit = 'C'
+        let cardTemplate = `
+                <div class = "card">
+                <img class = "card-img-top" src = "./icons/${icon}.png" alt = "${weatherCond}">
+                <div class = "card-body">
+                <h5 class = "card-title">${city},${country}</h5>
+                <p class = "card-text">${temp}&#176;${unit}</p>
+                <a href = "#" class = "btn btn-primary">Details</a>
+                </div>
+                </div>`
+        console.log(cardTemplate)
+}
