8466b8edb9b4cc971fb8c1c9f47a69018da7f457
alteregoxiv1 <prasad.biswasxiv@gmail.com>
2020-10-27 14:57:23 +0000

    card removed temp upd2

--- weather-guide-web/javascripts/tempconverter.js
+++ weather-guide-web/javascripts/tempconverter.js
@@ -17,21 +17,18 @@ function fahrenheitToCelcius(temp){
 
 function convertAllTemp() {
         let temps = document.getElementsByClassName('temp')
-        console.log(temps)
-        console.log(temps[0].innerHTML)
         let mode = document.getElementById("unit").checked
-        console.log(mode)
-        // for(let temp of temps){
+        for(let temp of temps){
 
-        //     console.log(temp)
-        //     let i = temp.innerHTML.split('°')[0]
-        //     console.log(i)
-        //     if(!mode)
-        //         temp.innerHTML = fahrenheitToCelcius(parseFloat(i)) + "°C"
-        //     else
-        //         temp.innerHTML = celciusToFahrenheit(parseFloat(i)) + "°F"
+            console.log(temp)
+            let i = temp.innerHTML.split('°')[0]
+            console.log(i)
+            if(mode)
+                temp.innerHTML = fahrenheitToCelcius(parseFloat(i)) + "°C"
+            else
+                temp.innerHTML = celciusToFahrenheit(parseFloat(i)) + "°F"
 
-        // }
+        }
         console.log(this)
         // ...
 }
