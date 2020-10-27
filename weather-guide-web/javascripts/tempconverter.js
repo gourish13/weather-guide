d9a3c9c1f814d56630dc01d13ab4388a9f533f80
alteregoxiv1 <prasad.biswasxiv@gmail.com>
2020-10-27 14:44:04 +0000

    tempcov upd1

--- weather-guide-web/javascripts/tempconverter.js
+++ weather-guide-web/javascripts/tempconverter.js
@@ -3,7 +3,7 @@ function kelvinToCelcius(tempInKelvin) {
 }
 
 function kelvinToFahrenheit(tempInKelvin) {
-    return Math.round((((tempInKelvin - 273.5) * (9 / 5)) + 32))
+    return Math.round((((tempInKelvin - 273.15) * (9 / 5)) + 32))
 }
 
 function celciusToFahrenheit(temp){
@@ -11,11 +11,27 @@ function celciusToFahrenheit(temp){
 }
 
 function fahrenheitToCelcius(temp){
+        return Math.round((temp - 32) * (5 / 9))
         // ...
 }
 
 function convertAllTemp() {
         let temps = document.getElementsByClassName('temp')
         console.log(temps)
+        console.log(temps[0].innerHTML)
+        let mode = document.getElementById("unit").checked
+        console.log(mode)
+        // for(let temp of temps){
+
+        //     console.log(temp)
+        //     let i = temp.innerHTML.split('°')[0]
+        //     console.log(i)
+        //     if(!mode)
+        //         temp.innerHTML = fahrenheitToCelcius(parseFloat(i)) + "°C"
+        //     else
+        //         temp.innerHTML = celciusToFahrenheit(parseFloat(i)) + "°F"
+
+        // }
+        console.log(this)
         // ...
 }
