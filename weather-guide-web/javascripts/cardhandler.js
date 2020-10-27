62ec69b850e9a745b40891f5e62576a4d57b0112
alteregoxiv1 <prasad.biswasxiv@gmail.com>
2020-10-27 15:47:37 +0000

    Card updetu

--- weather-guide-web/javascripts/cardhandler.js
+++ weather-guide-web/javascripts/cardhandler.js
@@ -1,7 +1,15 @@
 function addCityCard() {
+        if(apiData.length == 8){
+
+                alert("City Slots are full...Kindly remove one to open up a slot...")
+                return
+
+        }
+        
         let index = apiData.length;
         
         // Remove and start writing
+        setCard(cards[index])
         
         apiData[apiData.length] = 1;
 }
