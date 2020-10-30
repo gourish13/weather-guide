e42563cdb8e6e3a07a2d5e3e34f4817e335fdd66
Gourish Sadhu <sadhu5gourish@outlook.com>
2020-10-31 03:04:25 +0530

    experimenting with chart.js

--- weather-guide-web/javascripts/createchart.js
+++ weather-guide-web/javascripts/createchart.js
@@ -1,14 +1,21 @@
 function renderChart() {
 	const ctx = document.getElementById('chart').getContext('2d');
 	const chart = new Chart(ctx, {
-		type: 'bar',
+		type: 'line',
 		data: {
 			labels: ['1', '2', '3', '4', '5', '6'],
 			datasets: [{
 				label: '# of something',
 				data: [80, 20, 30, 36, 100, 130],
-				backgroundColor: 'yellow',
+				backgroundColor: 'transparent',
 				borderColor: 'red',
+				borderWidth: 2
+			},
+			{
+				label: '# of anotherthing',
+				data: [85, 120, 130, 63, 90, 110],
+				backgroundColor: 'transparent',
+				borderColor: 'green',
 				borderWidth: 2
 			}]
 		},
